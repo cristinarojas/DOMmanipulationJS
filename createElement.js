b4072e93e1e21d93f207b85a4b19a2fd7db1108a
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-08-10 13:35:18 -0500

    Adding changes

--- createElement.js
+++ createElement.js
@@ -1,12 +1,32 @@
 
 // Create element
-function myFunction() {
+function createButton() {
+
+	// Creating a button on the body
     var button = document.createElement("button");
     var textNode = document.createTextNode('Im button');
     var classAttribute = document.createAttribute('class');
-	classAttribute.value = 'my-class';    
+	classAttribute.value = 'my-button';    
 
     document.body.appendChild(button);
     button.appendChild(textNode);
     button.setAttributeNode(classAttribute);
+}
+
+// Create paragraph inside div
+function createParagraph() {
+
+	// Getting the div
+	var theDiv = document.getElementById('paragraph');
+	
+	// Creating the text node
+	var textNode = document.createTextNode('Im the paragraph');
+
+	// Creating the paragraph
+	var theP = document.createElement('p');
+
+	// Adding the text node to the paragraph
+	theP.appendChild(textNode);
+
+	theDiv.appendChild(theP);
 }
